8b28a5706928121ec74c618d17c47f6e7c272ef8
klenwell <klenwell@gmail.com>
2015-04-10 07:37:41 -0700

    Adds interviews index view.

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -6,6 +6,10 @@ class Interview < ActiveRecord::Base
   validates :company, :date, presence: true
   validates :culture, :people, :work, :career, :commute, :salary, :gut, inclusion: 1..5
 
+  # Scopes
+  scope :recent, ->{ order('DATE desc') }
+
+  # Public Methods
   def total
     [culture, people, work, career, commute, salary, gut].sum
   end
---
+++ test/controllers/interviews_controller_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class InterviewsControllerTest < ActionController::TestCase
+  setup do
+    @interview = interviews(:acme)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:interviews)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create interview" do
+    assert_difference('Interview.count') do
+      post :create, interview: { career: @interview.career, commute: @interview.commute,
+        company: @interview.company, culture: @interview.culture, date: @interview.date,
+        gut: @interview.gut, people: @interview.people, recruiter_id: @interview.recruiter_id,
+        salary: @interview.salary, work: @interview.work }
+    end
+
+    assert_redirected_to interview_path(assigns(:interview))
+  end
+
+  test "should show interview" do
+    get :show, id: @interview
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @interview
+    assert_response :success
+  end
+
+  test "should update interview" do
+    patch :update, id: @interview, interview: { career: @interview.career,
+      commute: @interview.commute, company: @interview.company, culture: @interview.culture,
+      date: @interview.date, gut: @interview.gut, people: @interview.people,
+      recruiter_id: @interview.recruiter_id, salary: @interview.salary, work: @interview.work }
+    assert_redirected_to interview_path(assigns(:interview))
+  end
+
+  test "should destroy interview" do
+    assert_difference('Interview.count', -1) do
+      delete :destroy, id: @interview
+    end
+
+    assert_redirected_to interviews_path
+  end
+end
