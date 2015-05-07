5c689de7fe25c63c163bc5eb98473b9a9a0895cd
klenwell <klenwell@gmail.com>
2015-05-06 22:19:06 -0700

    Adds new view for recruiters list.

--- app/controllers/recruiter_lists_controller.rb
+++ app/controllers/recruiter_lists_controller.rb
@@ -7,11 +7,29 @@ class RecruiterListsController < ApplicationController
 
   # GET /lists/new
   def new
-    render text: "new"
+    @list = RecruiterList.new
   end
 
   # POST /lists
   # POST /lists.json
   def create
+    @list = RecruiterList.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to lists_path, notice: 'List was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def list_params
+    params.require(:recruiter_list).permit(:name)
   end
 end
